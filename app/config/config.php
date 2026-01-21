de7fa37eee91149eb27c236d440d59d969723ea4
lptipmvps <lptipm.vps@gmail.com>
2026-01-21 11:41:52 +0700

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -1,7 +1,7 @@
 <?php
 
-define('DB_HOST', 'localhost');
-define('DB_PORT', '5432'); 
-define('DB_USER', 'postgres'); 
-define('DB_PASS', '123');
-define('DB_NAME', 'pmWeb');
+define('DB_HOST', getenv('DB_HOST') ?: 'localhost');
+define('DB_PORT', getenv('DB_PORT') ?: '5432');
+define('DB_USER', getenv('DB_USER') ?: 'postgres');
+define('DB_PASS', getenv('DB_PASS') ?: '123');
+define('DB_NAME', getenv('DB_NAME') ?: 'pmWeb');
